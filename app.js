49322c7b845dd12ee7ba4278b076e6692b9de0cc
Tom <tom.fleming@live.co.uk>
2022-07-26 16:08:20 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// ON KEYDOWN
+
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 49) {
+        document.getElementById('audio1').play();
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 50) {
+        document.getElementById('audio2').play();
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 51) {
+        document.getElementById('audio3').play();
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 52) {
+        document.getElementById('audio4').play();
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 53) {
+        document.getElementById('audio5').play();
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 54) {
+        document.getElementById('audio6').play();
+    }
+});
